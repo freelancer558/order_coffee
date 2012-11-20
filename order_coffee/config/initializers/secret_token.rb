b8767ea057e1b7d55422042ae678726466bb2855
paonoppon <paonoppon@gmail.com>
2012-11-20 15:55:31 +0700

    initial commit

---
+++ order_coffee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderCoffee::Application.config.secret_token = 'ddbad45ed80aff74005189cfdd850f157d335d770ebe2ad17accc041304991ecaacdeb03e5e13afc660e149467d82269a61e0d0ae13cd98480a96dc263645fdd'
