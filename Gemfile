b97cfeb6401b297a7389ab805f78ead6c20ceb15
paonoppon <paonoppon@gmail.com>
2012-11-23 13:57:28 +0700

    add image to coffee table

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bootstrap-sass'
 
 gem 'will_paginate', '~> 3.0'
 
+gem "paperclip", "~> 3.0"
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121121100607) do
+ActiveRecord::Schema.define(:version => 20121123060051) do
 
   create_table "coffs", :force => true do |t|
     t.string   "name"
     t.integer  "price"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "orders", :force => true do |t|
