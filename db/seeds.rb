3dfb3b76f4e7d01e283764f912f5a76ce693a6c5
paonoppon <paonoppon@gmail.com>
2012-11-23 16:50:39 +0700

    update privacy for Order view

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,10 @@ end
 #   { :name => 'VIP' }
 # ], :without_protection => true)
 # puts 'SETTING UP DEFAULT USER LOGIN'
-# user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
-# puts 'New user created: ' << user.name
-# user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
-# puts 'New user created: ' << user2.name
-# user.add_role :admin
-# user2.add_role :VIP
+ user = User.create! :name => 'Admin', :email => 'admin@admin.com', :password => 'admin123', :password_confirmation => 'admin123'
+ puts 'New user created: ' << user.name
+ user2 = User.create! :name => 'Pao', :email => 'pao@iwa.fi', :password => 'paopao123', :password_confirmation => 'paopao123'
+ puts 'New user created: ' << user2.name
+ user.add_role :admin
+ user2.add_role :VIP
+
